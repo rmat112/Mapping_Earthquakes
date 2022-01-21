27e1f706bc007fd425da0b65b86e10ce09d683ae
Ruchi Mathur <ruchim3@gmail.com>
2022-01-20 17:04:37 -0800

    updating logic and css for legend

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -99,6 +99,31 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
           }).addTo(earthquakes);
           // Then we add the earthquake layer to our map.
           earthquakes.addTo(map);
+
+          // Create a legend control object
+          let legend = L.control({position: 'bottomright'});
+
+          legend.onAdd = function () {
+            let div = L.DomUtil.create('div', 'info legend');
+            const magnitudes = [0, 1, 2, 3, 4, 5];
+            const colors = [
+              "#98ee00",
+              "#d4ee00",
+              "#eecc00",
+              "#ee9c00",
+              "#ea822c",  
+              "#ea2c2c"
+            ];              
+  
+            // loop through our intervals and generate a label with a colored square for each interval
+            for (var i = 0; i < magnitudes.length; i++) {
+                div.innerHTML +=
+                    '<i style="background:' + colors[i] + '"></i> ' +
+                    magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
+            }
+            return div;
+          };
+          legend.addTo(map);
         });
        
 
